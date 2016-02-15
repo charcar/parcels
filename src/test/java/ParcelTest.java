1e373810b7f79fe1f6e7cb14bfddc2fc59036828
Charles Carter <student@epicodus.com>
2016-02-15 13:17:39 -0800

    initialize files for new parcel project

---
+++ src/test/java/ParcelTest.java
@@ -0,0 +1,57 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class ParcelTest {
+
+  @Test
+  public void newParcel_instantiatesParcelCorrectly() {
+    
+  }
+
+  // @Test
+  // public void newTriangle_instantiatesCorrectly() {
+  //   Triangle testTriangle = new Triangle(2, 2, 2);
+  //   assertEquals(true, testTriangle instanceof Triangle);
+  // }
+  //
+  // @Test
+  // public void newTriangle_setsSide1() {
+  //   Triangle testTriangle = new Triangle(2, 2, 2);
+  //   assertEquals(2, testTriangle.getSide1());
+  // }
+  // @Test
+  // public void newTriangle_setsSide2() {
+  //   Triangle testTriangle = new Triangle(2, 2, 2);
+  //   assertEquals(2, testTriangle.getSide2());
+  // }
+  // @Test
+  // public void newTriangle_setsSide3() {
+  //   Triangle testTriangle = new Triangle(2, 2, 2);
+  //   assertEquals(2, testTriangle.getSide3());
+  // }
+  //
+  // @Test
+  // public void isEquilateral_whenAllSidesEqual_true() {
+  //   Triangle testTriangle = new Triangle(3, 3, 3);
+  //   assertEquals(true, testTriangle.isEquilateral());
+  // }
+  //
+  // @Test
+  // public void isIsosceles_whenTwoSidesEqual_true() {
+  //   Triangle testTriangle = new Triangle(4, 4, 5);
+  //   assertEquals(true, testTriangle.isIsosceles());
+  // }
+  //
+  // @Test
+  // public void isScalene_whenNoSidesEqual_true() {
+  //   Triangle testTriangle = new Triangle(3, 4, 5);
+  //   assertEquals(true, testTriangle.isScalene());
+  // }
+  //
+  // @Test
+  // public void isNotATriangle_whenSumOfTwoSidesIsLessThanLengthOfThird_true() {
+  //   Triangle testTriangle = new Triangle(2, 2, 8);
+  //   assertEquals(true, testTriangle.isNotTriangle());
+  // }
+
+}
